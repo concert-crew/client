3e6ab995c090636c13779dfee63f790ad840081b
Chantal Goethals <chantalgoethals@gmail.com>
2022-10-14 15:57:07 -0600

    Feat: building components from data tree

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./App.css"
+
+
+
+const App = () => {
+
+
+
+  return (
+    <main className='App'>
+      
+    </main>
+  )
+}
+
+export default App
---
+++ src/errorHandling/InternalServerError.js
@@ -0,0 +1,14 @@
+// import React from 'react';
+// import Status from '../Status/Status';
+
+// const InternalServerError = () => {
+//     return (
+//       <Status code={500}>
+//         <div>
+//           <h1>Internal Server Error</h1>
+//         </div>
+//       </Status>
+//     );
+//   };
+
+// export default InternalServerError;
