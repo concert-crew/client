fc1838b53aac972b9232373cbdea5e1f64eacb20
Chantal Goethals <chantalgoethals@gmail.com>
2022-10-16 00:14:12 -0600

    Feat: add status code 500 page

--- src/errorHandling/InternalServerError.js
+++ src/errorHandling/InternalServerError.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Status from './Status';
-
+import './InternalServerError.css';
+import './Status404.css'
 
 const InternalServerError = () => {
     return (
       <Status code={500}>
-        <div>
-          <h2>The server is down at the moment. . .</h2>
+        <div className='error-505-container'>
+          <h2 className='sorry'>The server is down at the moment. . .</h2>
         </div>
       </Status>
     );
