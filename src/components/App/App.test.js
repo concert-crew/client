83331a635c613c025d163f08533a71c5bf31cf2e
Chantal Goethals <chantalgoethals@gmail.com>
2022-10-19 19:08:04 -0600

    Feat:add cci test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,6 +1,11 @@
 import { render, screen } from '@testing-library/react';
+import Status404 from '../../errorHandling/Status404';
 import App from './App';
 
 test('renders the landing page', () => {
   render(App);
+});
+
+test('shows animations', () => {
+  render(Status404);
 });
--- src/components/Mutation/Mutation.js
+++ src/components/Mutation/Mutation.js
@@ -1,10 +1,14 @@
-import React from 'react'
-import { gql, useMutation } from "@apollo/client"
+import React from "react";
+import { gql, useMutation } from "@apollo/client";
 
 const CREATE_EVENT = gql`
-    mutation CreateEvent($name: String!, $ticketmasterId: String!, $butTicketsUrl: String!, $image: String!, $date: String!, $time: String!, $venueName: String!, $city: String!, $state: String!, $address: String!, $longitude: $String!, $latitude: $String!) {
+    mutation CreateEvent($name: String!, $ticketmasterId: String!, $butTicketsUrl: String!, 
+    $image: String!, $date: String!, $time: String!, $venueName: String!, $city: String!, 
+    $state: String!, $address: String!, $longitude: String!, $latitude: String!) {
         createEvent(input: {
-            name: $name, ticketmasterId: $ticketmasterId, buyTicketsUrl: $buyTicketsUrl, image: $image, date: $date, time: $time, venueName: $venueName, city: $city, state: $state, address: $address, longitude: $longitude, latitude: $latitude}) {
+            name: $name, ticketmasterId: $ticketmasterId, buyTicketsUrl: $buyTicketsUrl, 
+            image: $image, date: $date, time: $time, venueName: $venueName, city: $city, 
+            state: $state, address: $address, longitude: $longitude, latitude: $latitude}) {
        
             name
             ticketmasterId
@@ -21,25 +25,24 @@ const CREATE_EVENT = gql`
         
         }
     }
-`
+`;
 
 export const Mutation = (event) => {
-    console.log(event)
-    const [createEvent, {data, loading, error}] = useMutation(CREATE_EVENT, {
-        variables: {
-            name: event.name,
-            ticketmasterId: event.ticketmasterId,
-            buyTicketsUrl: event.buyTicketsUrl,
-            image: event.image,
-            date: event.date,
-            time: event.time,
-            venueName: event.venueName,
-            city: event.city,
-            state: event.state,
-            address: event.address,
-            longitude: event.longitude,
-            latitude: event.latitude
-        }
-    })
-}
-
+  console.log(event);
+  const [createEvent, { data, loading, error }] = useMutation(CREATE_EVENT, {
+    variables: {
+      name: event.name,
+      ticketmasterId: event.ticketmasterId,
+      buyTicketsUrl: event.buyTicketsUrl,
+      image: event.image,
+      date: event.date,
+      time: event.time,
+      venueName: event.venueName,
+      city: event.city,
+      state: event.state,
+      address: event.address,
+      longitude: event.longitude,
+      latitude: event.latitude,
+    },
+  });
+};
