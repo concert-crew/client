3eac89af84b1f17f90ddf904ac3f8c9a8e711f8a
jmedina <ndgns2@gmail.com>
2022-10-24 14:33:15 -0600

    troubleshooting circle

--- src/components/EventDetails/EventDetails.js
+++ src/components/EventDetails/EventDetails.js
@@ -62,7 +62,7 @@ const EventDetails = ({ event, user, setCurrentUser }) => {
     createEvent({
       variables: {
         input: 
-        {... event}
+        {...event}
         // {
         //   name: event.name,
         //   ticketmasterId: event.ticketmasterId,
