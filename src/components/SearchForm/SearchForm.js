f1189b0319bdb96a76f38fd133e7e4e787be2d43
Chantal Goethals <chantalgoethals@gmail.com>
2022-10-24 15:38:38 -0600

    Feat: add error handling for search form

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,51 +1,60 @@
-import React, { useState } from 'react'
-import "./SearchForm.css"
-import EventsContainer from "../EventsContainer/EventsContainer"
+import React, { useState } from "react";
+import "./SearchForm.css";
+import EventsContainer from "../EventsContainer/EventsContainer";
 
-const SearchForm = ({setSearchedEvents}) => {
-const [artistName, setArtistName] = useState("");
-const [results, setResults] = useState([])
+const SearchForm = ({ setSearchedEvents }) => {
+  const [artistName, setArtistName] = useState("");
+  const [results, setResults] = useState([]);
+  const [message, setMessage] = useState("Search for an upcoming show!");
 
-const handleChange = (e) => {
-  setArtistName(e.target.value)
+  const handleChange = (e) => {
+    setArtistName(e.target.value);
+  };
 
-}
+  const handleClick = (e) => {
+    e.preventDefault();
+    artistName
+      ? fetch(
+          `https://concert-crew-be-v2.herokuapp.com/api/v1/events?keyword=${artistName}`
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(`data.data.events`, data.errors);
+            if (!data.errors) {
+              setResults(data.data.events);
+              setSearchedEvents(data.data.events);
+              setMessage("");
+            } else {
+              setMessage("No events found . . .");
+            }
+          })
+      : setMessage("Please type in an artist");
+    setArtistName("");
+  };
 
-
-const handleClick = (e) => {
-  e.preventDefault()
-
-  fetch(`https://concert-crew-be-v2.herokuapp.com/api/v1/events?keyword=${artistName}`)
-  .then(response => response.json())
-  .then(data => {
-    setResults(data.data.events)
-    setSearchedEvents(data.data.events)
-  })
-  setArtistName('')
-}
-
-
-const toBeDisplayed = results.length ?  
-<EventsContainer events={results}/> 
-: <p>Search for an upcoming show!</p>
+  const result = results.length ? <EventsContainer events={results} /> : null;
 
   return (
     <div>
-    <form>
-       <input 
-       type='text'
-       placeholder='Artist Name'
-       name='artist-name'
-       value={artistName}
-       onChange= {e=>handleChange(e)}
-       />
-    <button className="search-btn" onClick={e=>handleClick(e)}> SEARCH </button>
-    </form>
+      <form>
+        <input
+          type="text"
+          placeholder="Artist Name"
+          name="artist-name"
+          value={artistName}
+          onChange={(e) => handleChange(e)}
+        />
+        <button className="search-btn" onClick={(e) => handleClick(e)}>
+          {" "}
+          SEARCH{" "}
+        </button>
+      </form>
       <div className="search-results-container">
-        {toBeDisplayed}
+        {message}
+        {result}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SearchForm
+export default SearchForm;
